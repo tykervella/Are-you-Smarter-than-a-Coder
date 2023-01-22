ea98eb774632e253650d576522f6e63fb637ea2d
tykervella <kervellatyler@gmail.com>
2023-01-22 02:29:53 -0800

    finished questionset array with actual content. added clarifying comments to remaining code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,17 @@ var li2 = document.querySelector('#B');
 var li3 = document.querySelector('#C');
 var li4 = document.querySelector('#D');
 
+// grabbing local data and displaying stored high score
+var highscore = localStorage.getItem("High Score");
+textElement.textContent = "High Score: " + highscore;
+
+
 // global variables thatre true at the start of each game. Will be manipulated as other functions later. 
 var timeLeft = 60;
 var score = 0;
 var currentQuestionIndex = 0; 
 
-//Setting Attributes of some list elements to make them easier to select
+//Setting Attributes of some list elements to it easier for user to select the option they want
 
 li1.setAttribute("Style","background-color: var(--accent2);")
 li3.setAttribute("Style","background-color: var(--accent2);")
@@ -32,71 +37,71 @@ var questionset = [
         "D": "HTML",
         "Answer": "B",
     }, { 
-        "Question": "Question2",
-        "A": "Text",
-        "B": "Correct",
-        "C": "Text",
-        "D": "Text",
+        "Question": "What is the correct syntax of doctype in HTML?",
+        "A": "<doctype html>",
+        "B": "<doctype html!>",
+        "C": "<!doctype html>",
+        "D": "</doctype html>",
+        "Answer": "C",
+    }, {   
+        "Question": "What is the purpose of CSS in web development?",
+        "A": "To define the structure of a webpage",
+        "B": "To give interactivity to a webpage",
+        "C": "To stylize how a webpage will look",
+        "D": "To prevent viruses when viewing a webpage",
+        "Answer": "C",
+    }, {   
+        "Question": "How many levels of headers are available in HTML?",
+        "A": "h1-h4",
+        "B": "h1-h6",
+        "C": "h1-h8",
+        "D": "h1-h10",
         "Answer": "B",
     }, {   
-        "Question": "Question3",
-        "A": "Text",
-        "B": "Text",
-        "C": "Correct",
-        "D": "Text",
-        "Answer": "C",
-    }, {   
-        "Question": "Question4",
-        "A": "Text",
-        "B": "Text",
-        "C": "Text",
-        "D": "Correct",
+        "Question": "What is the proper way to iterate a function is JS?",
+        "A": "for (var i = 1; i > length; i++) {}",
+        "B": "for (var i = 1; i < length; i++) {}",
+        "C": "for (var i = 0; i > length; i++) {}",
+        "D": "for (var i = 0; i < length; i++) {}",
         "Answer": "D",
     }, {   
-        "Question": "Question5",
-        "A": "Text",
-        "B": "Correct",
-        "C": "Text",
-        "D": "Text",
+        "Question": "Where are metadata tags contained in HTML",
+        "A": "<HTML>",
+        "B": "<Head>",
+        "C": "<Body>",
+        "D": "<Header>",
         "Answer": "B",
     }, {   
-        "Question": "Question6",
-        "A": "Text",
-        "B": "Text",
+        "Question": "What type of display style in CSS allows for attributes to resize and fit to the current size of a webpage?",
+        "A": "Flex",
+        "B": "Stretch",
+        "C": "Grow",
+        "D": "Resize",
+        "Answer": "A",
+    }, {   
+        "Question": "Which of the following is not a type of primitive that is used in JS",
+        "A": "String",
+        "B": "Boolean",
         "C": "Text",
-        "D": "Correct",
+        "D": "Number",
+        "Answer": "C",
+    }, {   
+        "Question": "How do you link to your javascript file in HTML?",
+        "A": "<script link='./assets/script.js'></script>",
+        "B": "<script href='./assets/script.js'></script>",
+        "C": "<script src='./assets/script.js'></script>",
+        "D": "<script source='./assets/script.js'></script>",
+        "Answer": "C",
+    }, {   
+        "Question": "Is web development fun?",
+        "A": "No >:(",
+        "B": "Maybe...",
+        "C": "Sometimes ;/",
+        "D": "YES!!!!!!!!!!!!!!!!",
         "Answer": "D",
-    }, {   
-        "Question": "Question7",
-        "A": "Correct",
-        "B": "Text",
-        "C": "Text",
-        "D": "Text",
-        "Answer": "A",
-    }, {   
-        "Question": "Question8",
-        "A": "Text",
-        "B": "Text",
-        "C": "Correct",
-        "D": "Text",
-        "Answer": "C",
-    }, {   
-        "Question": "Question9",
-        "A": "Text",
-        "B": "Text",
-        "C": "Correct",
-        "D": "Text",
-        "Answer": "C",
-    }, {   
-        "Question": "Question10",
-        "A": "Correct",
-        "B": "Text",
-        "C": "Text",
-        "D": "Text",
-        "Answer": "A",
     }, 
 ];
-
+// variable to store how many questions are in the quiz
 qlength = questionset.length;
 
 // function that start the countdown event. 
@@ -104,14 +109,14 @@ function countdown() {
     startbutton.setAttribute("style","display:none;");
 
     var timeInterval = setInterval(function() {
-//      Occurs when game is over. Calls score display, which I only want to occur after the game has ended. 
+//Occurs when game is over. Calls score display, which I only want to occur after the game has ended. 
         if (timeLeft <= 0) {
             clearInterval(timeInterval);
             timerElement.textContent = "Game Over!";
             gameboxelement.style.display = "none";
             displayScore();
             return; 
-        // Countdown by 1s every 1000 ms 
+        // Countdown by 1 every 1000 ms 
         } else if (timeLeft >= 1) {
             timerElement.textContent = "Time Remaining: \n" + timeLeft + "s";
             timeLeft = (timeLeft - 1)
@@ -148,6 +153,7 @@ function checkAnswer(input) {
         score++;
         textElement.textContent = "You got that question right!! Nice job!";
     } else {
+        // If wrong, lose 5 seconds. 
         timeLeft = (timeLeft - 5);
         textElement.textContent = "You got the last question wrong! Better luck next time";
     }
@@ -158,20 +164,22 @@ function checkAnswer(input) {
 }
 
 
+
+// Displays final score. Shows button that allows user to store score in localstorage
 function displayScore() {
     savescore.setAttribute("style","display:block;");
     textElement.textContent = "Final score: " + score;
 }
 
 
-
+// event listener for start button.  
 startbutton.addEventListener("click", function () {
     countdown();
+    //makes 1st question display inbox
     gameboxelement.setAttribute("style","display:inline;");
-
-
     gameRender(currentQuestionIndex);
 
+// creates event listener for each multiple choice option and checks if the user chose the correct answer. 
     li1.addEventListener("click", function(){
         checkAnswer("A");
     });
@@ -186,6 +194,8 @@ startbutton.addEventListener("click", function () {
     });
 } );
 
+
+// creates event listener for save button. Prompts user for their first and last initial and saves that input with their score in localstorage. 
 savescore.addEventListener("click", function(event){
     event.preventDefault();
     
