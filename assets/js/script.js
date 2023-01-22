94720dbc68ec033e376896620f71ceb1af80fb63
tykervella <kervellatyler@gmail.com>
2023-01-22 00:13:07 -0800

    coded for iteration through the questions matrix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,19 @@ var timerElement = document.querySelector('#countdown');
 var buttonElement = document.querySelector('#game-start');
 var gameboxelement = document.querySelector('#question-box');
 var questionElement = document.querySelector('#question');
-var choicesElement = document.querySelector('#options');
-var li1 = document.querySelector('#a');
-var li2 = document.querySelector('#b');
-var li3 = document.querySelector('#c');
-var li4 = document.querySelector('#d');
+var answersElement = document.querySelector('#options');
+var textElement = document.querySelector('#text-channel');
+var li1 = document.querySelector('#A');
+var li2 = document.querySelector('#B');
+var li3 = document.querySelector('#C');
+var li4 = document.querySelector('#D');
 
-// global variable to set the countdown for the game to start at 60 seconds and be manipulated by multiple units; 
+// global variables thatre true at the start of each game. Will be manipulated as other functions later. 
 var timeLeft = 60;
+var score = 0;
+var currentQuestionIndex = 0; 
 
-// Creates an array that is able to store each question as an object that properties are grabbed and then used for various functions 
+// Creates an array that is able to store each question as an object. Properties are grabbed and then used for various functions to populate the quiz box and check the user input vs. the correct answer.
 var questionset = [
     { 
         "Question": "Which of the following languages is not considered on of the 3 fundamental building blocks of webpages?",
@@ -25,7 +28,7 @@ var questionset = [
     }, { 
         "Question": "Question2",
         "A": "Text",
-        "B": "Text",
+        "B": "Correct",
         "C": "Text",
         "D": "Text",
         "Answer": "B",
@@ -33,7 +36,7 @@ var questionset = [
         "Question": "Question3",
         "A": "Text",
         "B": "Text",
-        "C": "Text",
+        "C": "Correct",
         "D": "Text",
         "Answer": "C",
     }, {   
@@ -41,12 +44,12 @@ var questionset = [
         "A": "Text",
         "B": "Text",
         "C": "Text",
-        "D": "Text",
+        "D": "Correct",
         "Answer": "D",
     }, {   
         "Question": "Question5",
         "A": "Text",
-        "B": "Text",
+        "B": "Correct",
         "C": "Text",
         "D": "Text",
         "Answer": "B",
@@ -55,11 +58,11 @@ var questionset = [
         "A": "Text",
         "B": "Text",
         "C": "Text",
-        "D": "Text",
+        "D": "Correct",
         "Answer": "D",
     }, {   
         "Question": "Question7",
-        "A": "Text",
+        "A": "Correct",
         "B": "Text",
         "C": "Text",
         "D": "Text",
@@ -68,46 +71,56 @@ var questionset = [
         "Question": "Question8",
         "A": "Text",
         "B": "Text",
-        "C": "Text",
+        "C": "Correct",
         "D": "Text",
         "Answer": "C",
     }, {   
         "Question": "Question9",
         "A": "Text",
         "B": "Text",
-        "C": "Text",
+        "C": "Correct",
         "D": "Text",
         "Answer": "C",
     }, {   
         "Question": "Question10",
-        "A": "Text",
+        "A": "Correct",
         "B": "Text",
         "C": "Text",
         "D": "Text",
         "Answer": "A",
-    }, {   
-    }
+    }, 
 ];
+
+qlength = questionset.length;
 
 // function that start the countdown event. 
 function countdown() {
 
     var timeInterval = setInterval(function() {
 
-        if (timeLeft >= .1) {
+        if (timeLeft <= 0) {
+            clearInterval(timeInterval);
+            timerElement.textContent = "Game Over!";
+            gameboxelement.style.display = "none";
+            displayScore();
+            return; 
+        } else if (timeLeft >= 1) {
             timerElement.textContent = "Time Remaining: \n" + timeLeft + "s";
-            timeLeft = (timeLeft - 0.1).toFixed(1);
+            timeLeft = (timeLeft - 1)
         } else {
             timerElement.textContent = "Times Up!";
             clearInterval(timeInterval);
         }
-    }, 100);
+    }, 1000);
 };
 
 
 // function that renders the content of a specific index from the questionset array. 
 function gameRender(i) {
-
+    if (i >= qlength) {
+        timeLeft = 0;
+        return;
+    }
     questionElement.textContent = questionset[i].Question;
     li1.textContent = questionset[i].A;
     li2.textContent = questionset[i].B;
@@ -115,70 +128,125 @@ function gameRender(i) {
     li4.textContent = questionset[i].D;
 };
 
-
-// function that checks the player input and then returns whether they were correct or not. 
-function checkinput(i) {
-
-    var status = "";
-
-    if (status >1) {
-        li1.addEventListener("click", function(event) {
-            input = "A";
-            return 
-        });
-
-        li2.addEventListener("click", function(event) {
-            input = "B";
-
-            if (input === questionset[i].Answer) {
-                status++;
-            } else {
-                timeLeft = (timeLeft - 5);
-            }
-        });
-
-        li3.addEventListener("click", function(event) {
-            input = "C";
-
-            if (input === questionset[i].Answer) {
-                status++;
-            } else {
-                timeLeft = (timeLeft - 5);
-            }
-        });
-
-        li4.addEventListener("click", function(event) {
-            input = "D";
-
-            if (input === questionset[i].Answer) {
-                status++; 
-            } else {
-                timeLeft = (timeLeft - 5);
-            }
-        });
-    }  else {
-        return status; 
+function checkAnswer(input) {
+    // Get the selected answer
+    var selectedAnswer = input;
+    console.log (selectedAnswer);
+    // Get the correct answer from the current question object
+    var correctAnswer = questionset[currentQuestionIndex].Answer;
+    // Compare the selected answer to the correct answer
+    if (selectedAnswer === correctAnswer) {
+        // If correct, increment the score
+        score++;
+        currentQuestionIndex++;
+        textElement.textContent = "Nice job- you got that question right!! Let's try and keep the ball rolling!";
+    } else {
+        timeLeft = (timeLeft - 5);
+        textElement.textContent = "You got that last question wrong! Let's try again!";
     }
-};
+    // Move on to the next question
+    // Render the next question
+    gameRender(currentQuestionIndex);
+}
+
+
+function displayScore() {
+    textElement.textContent = "Your final score is: " + score;
+}
+
 
 
 buttonElement.addEventListener("click", function () {
-    gameboxelement.setAttribute("style","display:inline;")
-    countdown ();
+    countdown();
+    gameboxelement.setAttribute("style","display:inline;");
+
+
+    gameRender(currentQuestionIndex);
+
+    li1.addEventListener("click", function(){
+        checkAnswer("A");
+    });
+    li2.addEventListener("click", function(){
+        checkAnswer("B");
+    });
+    li3.addEventListener("click", function(){
+        checkAnswer("C");
+    });
+    li4.addEventListener("click", function(){
+        checkAnswer("D");
+    });
+} );
+
+
+// function checkvalue(i) {
+//     answersElement.addEventListener("click", function (event){
+//         var element = event.target; 
+//         var state = element.getAttribute("data-state");
+//         var value = element.getAttribute("data-value");
+//         var answer = questionset[i].Answer;
+
+//         var event;
+
+
+//         if (state==="chosen") {
+//             if (value === answer) {
+//                 event = true;
+//                 return event;
+//             } else {
+//                 timeLeft = (timeLeft - 5)
+//             }
+//         }
+
+
+//     })
+
+// }
 
 
 
-    for (var index=0; index <questionset.length; index++) {
-        gameRender(index);
-        var correct = checkinput (index);
-        
-        if (correct !=1) {
-            break;
-        } else {
-            return; 
-        }
+//     function checkInput (i) {
+//         gameRender(i);
+
+//         answersElement.addEventListener("click", function (event){
+//             var answer = questionset[i].Answer
+//             var element = event.target; 
+//             var value = element.getAttribute("data-value");
+                        
+                    
+//             if (element.matches("#A")) {
+//                 if (answer==value) {
+//                     console.log("Correct!!!")
+//                 } else {
+//                     timeLeft = (timeLeft - 5);
+//                 }
+//             }
+//         })
+                      
+//             // } else if (element.matches("#B")) {
+//             //     input= value;
+//             //     return input;            
+//             // } else if (element.matches("#C")) {
+//             //     input= value;
+//             //     return input;             
+//             // } else if (element.matches("#D")) {
+//             //     input= value;
+//             //     return input;                  
+//             // }
+//         // console.log (input)
+//         }
+
+    
+
+//     console.log(checkInput(0));
+// //         if (input == ("A" || "B" || "C" || "D")) {
+// //             console.log(input);
+// //             console.log('Im in this line of code');
+// //         } else {
+// //             
 
 
-    };
+//     }
+// }
 
-});
+
+
